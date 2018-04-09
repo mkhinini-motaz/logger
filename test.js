2e97abda3e514c819fdbd2007c130d4d33653256
Moatez Mkhinini <moatez@busineyss.com>
2018-04-09 17:07:32 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,18 @@
+// Testing all the methods
+// logger.isPersistingLogs = false; // ==> because you don't want these dummy logs in your log file
+// logger.log("Simple Log")
+// logger.error({ name: "SomeError", message: "The error description" });
+//
+// logger.info("info");
+// logger.info("info", true);
+//
+// logger.primary("primary");
+// logger.primary("primary", true);
+//
+// logger.success("success");
+// logger.success("success", true);
+//
+// logger.warning("warning");
+// logger.warning("warning", true);
+//
+// logger.isPersistingLogs = true;
